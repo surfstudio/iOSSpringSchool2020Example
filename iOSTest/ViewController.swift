724a099f46a76483789ce43aa9d016a1ef17b7ce
Krupenko Validislav <krupenko@surfstudio.ru>
2020-04-08 10:18:15 +0300

    Initial Commit

---
+++ iOSTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSTest
+//
+//  Created by Vladislav Krupenko on 08.04.2020.
+//  Copyright © 2020 Surf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
